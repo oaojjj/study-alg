02ea4bba83e396c1cb6addcd59c5f7680f5c4e73
oaojjj <oxojjjj@gmail.com>
2019-09-04 18:57:38 +0900

    update week1

---
+++ 자료구조 과제/PA_W01/02.c
@@ -0,0 +1,41 @@
+#include stdio.h
+#include string.h
+#include ctype.h
+
+#define BUFFER_SIZE 21  단어의 최대 길이는 20
+
+int main() {
+	int n, m;
+	int ch, i=0;
+	char array[80]; // m=9, n=9일때 필요한 배열의 크기 m x n=81
+	char buffer[BUFFER_SIZE]; // 각 단어를 임시로 저장할 배열이다.
+
+	scanf(%d %d, &m, &n);
+	getchar(); // (엔터키)버퍼비우기
+
+	while(imn){ // 입력된 행열의 수 만큼 반복한다.
+		
+		int index = 0;
+		while (1) { // 무한루프를 돌면서 모든 예외적인 상황을 처리한다.(숫자도 예외 포함)
+			ch = getchar();
+			if (isalpha(ch)) { // 입력받은 문자가 알파벳이라면 버퍼에 추가한다.
+				if (index  BUFFER_SIZE-1) {
+					buffer[index++] = ch;
+				}
+			}
+			else if (isspace(ch)) { // 모든 공백문자를 체크한다.
+				break;
+			}
+		}
+		if (index  0) { // 공백이 먼저 입력되거나 여러번 입력되었을 경우 인덱스를 체크하여 예외로 처리한다.
+			buffer[index] = '0';
+			array[i++] = strdup(buffer);
+		}
+	}
+	
+
+	for (int j = 0; j  mn; j++) {
+		printf(%s , array[j]);
+	}
+	return 0;
+}
