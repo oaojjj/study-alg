d5404de92fbeee685d0d913f859e3a616aadf1b8
OseongKwon <oxojjjj@gmail.com>
2019-09-17 16:42:45 +0900

    success w02_01

--- 자료구조 과제/PA_W02/01.c
+++ 자료구조 과제/PA_W02/01.c
@@ -10,12 +10,14 @@
 
 void my_strlwr(char* str);
 int isFirstWord(char*str[], char*buffer);
+void sort(char*str[], int count[]);
 
 int total_cnt; // 전체 단어 개수 저장
+int in_idx; // 첫 단어가 아닐 때 저장 되어있는 인덱스값(include index)
 
-	int main() {
+int main() {
 	char *word[MAX_WORD];
-	int word_count[MAX_WORD]; // 각 단어 개수 저장
+	int word_count[MAX_WORD] = { 0, }; // 각 단어 개수 저장
 	char buffer[BUF_LEN];
 	int index;
 	int ch;
@@ -62,27 +64,36 @@ int total_cnt; // 전체 단어 개수 저장
 			continue;
 		}
 
-		if (total_cnt == 0) { //첫 단어
-			word[0] = strdup(buffer);
-			word_count[0]++;
-			total_cnt++;
-		}
-		else { // 완성 단어 카운트
-			if (isFirstWord(word, buffer)) { // 첫 단어 검사
+		if (strlen(buffer) >= 7) {
+			if (total_cnt == 0) { //첫 단어
+				word[total_cnt] = strdup(buffer);
 				word_count[total_cnt]++;
 				total_cnt++;
 			}
-			else { // 첫 단어 아닐 때
-				for (int i = 0; i < total_cnt; i++) {
-					if (!strcmp(word[i], buffer)) {
-						word_count[i]++;
-						total_cnt++;
-						break;
-					}
+			else { // 완성 단어 카운트
+				if (isFirstWord(word, buffer)) { // 첫 단어 검사
+					word[total_cnt] = strdup(buffer);
+					word_count[total_cnt]++;
+					total_cnt++;
+				}
+				else { // 첫 단어 아닐 때
+					word_count[in_idx]++;
 				}
 			}
 		}
 	} // while
+
+	// 사전식 순서
+	sort(word,word_count);
+
+
+	// 출력
+	for (int i = 0; i < total_cnt; i+=10) {
+		printf("%s %d\n", word[i], word_count[i]);
+	}
+
+	fclose(fp);
+
 }
 
 // 소문자 변환 함수
@@ -96,13 +107,41 @@ void my_strlwr(char*str) {
 	}
 }
 
+// 사전식 순서
+void sort(char * str[],int count[]) {
+	char *str_tmp;
+	int cnt_tmp;
+	int index;
+
+	for (int i = 0; i < total_cnt - 1; i++) {
+		index = i;
+		for (int j = i + 1; j < total_cnt; j++) {
+			if (strcmp(str[index], str[j]) > 0) {
+				index = j;
+			}
+
+		}
+		// 단어
+		str_tmp = str[i];
+		str[i] = str[index];
+		str[index] = str_tmp;
+
+		// 카운트 숫자
+		cnt_tmp = count[i];
+		count[i] = count[index];
+		count[index] = cnt_tmp;
+	}
+}
+
 // 첫 단어 검사
 int isFirstWord(char * str[], char buffer[])
 {
 	int flag = TRUE;
 	for (int i = 0; i < total_cnt; i++) {
-		if (strcmp(str[i], buffer)) {
+		if (!strcmp(str[i], buffer)) {
 			flag = FALSE;
+			in_idx = i;
+			break;
 		}
 	}
 	return flag;
