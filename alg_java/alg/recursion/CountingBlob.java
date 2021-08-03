e6fb4a08c5b5a4d72826e7b5bdd02047658c539f
Oseong Kwon <oxojjjj@gmail.com>
2021-08-03 16:09:54 +0900

    Add files via upload

--- alg_java/alg/recursion/CountingBlob.java
+++ alg_java/alg/recursion/CountingBlob.java
@@ -1,5 +1,19 @@
 package alg.recursion;
 
+import static alg.PathSign.*;
+
+/*
+CountingBlob countingBlob = new CountingBlob(new int[][]{
+                {0, 0, 1, 1, 1},
+                {0, 0, 0, 1, 0},
+                {1, 1, 0, 0, 1},
+                {0, 1, 1, 0, 0},
+                {0, 0, 1, 0, 1},
+        }, 5);
+
+        int result = countingBlob.countBlob(0, 2);
+        System.out.println(result);
+ */
 public class CountingBlob {
     int[][] grid;
     int n;
@@ -9,13 +23,17 @@ public class CountingBlob {
         this.n = n;
     }
 
-    int countBlob(int x, int y) {
-
+    public int countBlob(int x, int y) {
         if (x < 0 || y < 0 || x >= n || y >= n) {  // out of grid size
             return 0;
-        } else if (grid[x][y] != 0) {  // not pixel or already count
+        } else if (grid[x][y] != PATHWAY.ordinal()) {  // not pixel or already count
             return 0;
+        } else { // exist pixel
+            grid[x][y] = PATH.ordinal();
+            Maze.printMaze(grid, n);
+            return 1 + countBlob(x - 1, y) + countBlob(x - 1, y + 1) + countBlob(x, y + 1) +
+                    countBlob(x + 1, y + 1) + countBlob(x + 1, y) + countBlob(x + 1, y - 1) +
+                    countBlob(x, y - 1) + countBlob(x - 1, y - 1);
         }
-        return 0;
     }
 }
--- alg_java/alg/recursion/Maze.java
+++ alg_java/alg/recursion/Maze.java
@@ -1,7 +1,18 @@
 package alg.recursion;
 
-import alg.Util.PathSign;
+import static alg.PathSign.*;
 
+/*
+Maze maze = new Maze(
+                new int[][]{
+                        {0, 0, 0, 0},
+                        {1, 1, 0, 1},
+                        {0, 0, 0, 1},
+                        {0, 1, 0, 0}}, 4);
+
+        maze.findMazePath(0, 0);
+        maze.printMaze();
+ */
 public class Maze {
     int[][] maze;
     int n;
@@ -14,21 +25,21 @@ public class Maze {
     public boolean findMazePath(int x, int y) {
         if (x < 0 || y < 0 || x >= n || y >= n) { // out of map size
             return false;
-        } else if (maze[x][y] != PathSign.PATHWAY.ordinal()) { // no path
+        } else if (maze[x][y] != PATHWAY.ordinal()) { // no path
             return false;
         } else if (x == n - 1 && y == n - 1) { // found exit
-            maze[x][y] = PathSign.EXIT.ordinal();
+            maze[x][y] = EXIT.ordinal();
             return true;
         } else {
-            maze[x][y] = PathSign.PATH.ordinal();
+            maze[x][y] = PATH.ordinal();
             if (findMazePath(x, y + 1) || findMazePath(x + 1, y) || findMazePath(x - 1, y) || findMazePath(x, y - 1))
                 return true;
-            maze[x][y] = PathSign.BLOCKED.ordinal();
+            maze[x][y] = BLOCKED.ordinal();
             return false;
         }
     }
 
-    public void printMaze() {
+    public static void printMaze(int[][] maze, int n) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print(maze[i][j] + " ");
