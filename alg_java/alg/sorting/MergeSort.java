4c29473123224cbd22d452da28d1bbcfd6e9b059
Oseong Kwon <oxojjjj@gmail.com>
2021-08-10 13:47:06 +0900

    add merge sort

--- alg_java/alg/sorting/MergeSort.java
+++ alg_java/alg/sorting/MergeSort.java
@@ -9,11 +9,31 @@ public class MergeSort {
         n = data.length;
     }
 
-    public void sort() {
-
+    public void sort(int start, int end) {
+        if (start < end) {
+            int middle = (end + start) / 2;
+            sort(start, middle); // left
+            sort(middle + 1, end); // right
+            merge(start, middle, end);
+        }
     }
 
-    private void merge() {
+    private void merge(int s, int m, int e) {
+        int k = s, i = s, j = m + 1;
+        int[] temp = new int[n];
 
+        // 왼쪽 루트와 오른쪽 루트중 한쪽의 값이 다 떨어질 때 까지 루프를 돌며 값 비교후 합병
+        while (i <= m && j <= e) {
+            if (data[i] <= data[j]) temp[k++] = data[i++];
+            else temp[k++] = data[j++];
+        }
+
+        // 한쪽의 값이 다 삽입 됬다면 다른 쪽의 남은 값도 삽입
+        while (i <= m) temp[k++] = data[i++];
+        while (j <= e) temp[k++] = data[j++];
+        
+        for (int index = s; index <= e; index++) {
+            data[index] = temp[index];
+        }
     }
 }
